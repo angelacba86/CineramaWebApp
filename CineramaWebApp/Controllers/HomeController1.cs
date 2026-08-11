02d5fa9d1adbe2bc54fcd35f158d0c6e25da10da
Angela Barrenechea <angelacba86@gmail.com>
2026-08-11 00:02:45 -0500

    prueba

---
+++ CineramaWebApp/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CineramaWebApp.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
