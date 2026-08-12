38be1594d998ff8d3078398efcaa2c2271fb2f36
Angela Barrenechea <angelacba86@gmail.com>
2026-08-12 14:08:39 -0500

    modularizar Detalles y pasar de viewBag a viewModel

---
+++ CineramaWebApp/Models/ViewModels/DetallePeliculaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CineramaWebApp.Models.DTOs;
+using CineramaWebApp.Models.Entities;
+
+namespace CineramaWebApp.Models.ViewModels
+{
+    public class DetallePeliculaViewModel
+    {
+        public PeliculaDTO Pelicula { get; set; } = default!;
+        public int IdCineSeleccionado { get; set; }
+        public string CiudadSeleccionada { get; set; } = string.Empty;
+        public DateTime FechaSeleccionada { get; set; } = DateTime.Today;
+
+        // Datos Paso 1
+        public IEnumerable<Cine> CinesDisponibles { get; set; } = Enumerable.Empty<Cine>();
+
+        // Datos Paso 2
+        public Cine? CineActual { get; set; }
+        public IEnumerable<DateTime> FechasDisponibles { get; set; } = Enumerable.Empty<DateTime>();
+        public IEnumerable<FuncionDisponibleDTO> Funciones { get; set; } = Enumerable.Empty<FuncionDisponibleDTO>();
+    }
+}
